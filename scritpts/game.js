ebbc20bd0f4f99e09a5605c6742e13ec5e54b61d
dr-mogambo <beepingtom@gmail.com>
2020-08-27 02:04:42 +0530

    adding simple first steps

--- scritpts/game.js
+++ scritpts/game.js
@@ -1,10 +1,29 @@
 (function () {
+    var canvas = document.getElementById('canvas'); //notice onn hover it detects its an html element
+    var stage;
+    var helloLabel;
     function Init() {
-        console.log('initialisatin started')
+        console.log('initialisation started....');
+        Start();
     }
-    function Start() { }
-    function Update() { }
-    function Main() { }
-
-    window.onload = Init
-})()
+    function Start() {
+        console.log("starting application");
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; //defines how many times a stage is redrawn
+        createjs.Ticker.on("tick", Update);
+        Main();
+    }
+    function Update() {
+        helloLabel.rotation += 5;
+        stage.update();
+    }
+    function Main() {
+        console.log('game started...');
+        helloLabel = new createjs.Text('Hello World', '40px Consolas', '#000000');
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
+    }
+    window.onload = Init;
+})();
+//# sourceMappingURL=game.js.map
---
+++ scritpts/game.ts
@@ -0,0 +1,31 @@
+(function () {
+    let canvas = document.getElementById('canvas')//notice onn hover it detects its an html element
+    let stage: createjs.Stage;
+    let helloLabel: createjs.Text;
+
+    function Init(): void {
+        console.log('initialisation started....')
+        Start()
+
+    }
+    function Start(): void {
+        console.log("starting application")
+        stage = new createjs.Stage(canvas)
+        createjs.Ticker.framerate = 60 //defines how many times a stage is redrawn
+        createjs.Ticker.on("tick", Update)
+        Main()
+    }
+    function Update(): void {
+        helloLabel.rotation += 5
+        stage.update()
+    }
+    function Main(): void {
+        console.log('game started...')
+        helloLabel = new createjs.Text('Hello World', '40px Consolas', '#000000')
+        helloLabel.x = 100
+        helloLabel.y = 100
+        stage.addChild(helloLabel)
+    }
+
+    window.onload = Init
+})()
